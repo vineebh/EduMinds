12023fc7077c9eedb6f0690002d441638ab8e35d
Kuldeep2804Yadav <kuldeep2804yadav@gmail.com>
2024-09-15 00:00:26 +0530

    Added Responsive design

--- FrontEnd/src/components/Auth/Auth.js
+++ FrontEnd/src/components/Auth/Auth.js
@@ -6,7 +6,7 @@ import { signInUserEmailAndPass, createUserEmailAndPass, signOutUser, signInWith
 
 const Auth = () => {
   const isLogin = useSelector((state) => state.auth.islogin);
-  const navigate= useNavigate();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const [authData, setAuthData] = useState({
     name: "",
@@ -33,7 +33,6 @@ const Auth = () => {
         navigate('/home')
       } else {
         await createUserEmailAndPass(email, password);
-        // Optionally handle post-signup actions
       }
       dispatch(setIsLogin(true));
     } catch (error) {
@@ -70,83 +69,77 @@ const Auth = () => {
   };
 
   return (
-    <div>
-
-      <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-        <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-          <h1 className="text-3xl font-bold text-blue-800 mb-6 text-center">
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center p-4">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl">
+        <h1 className="text-3xl font-bold text-blue-800 mb-6 text-center">
+          {isLogin ? "Login" : "Signup"}
+        </h1>
+        <form onSubmit={handleSubmit}>
+          {!isLogin && (
+            <div className="mb-4">
+              <label htmlFor="name" className="block text-gray-800 font-semibold mb-2">
+                Full Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Full Name"
+                value={authData.name}
+                onChange={handleChange}
+                className="w-full p-2 border border-gray-300 rounded-md bg-gray-50 text-black focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+          )}
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-800 font-semibold mb-2">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Your Email"
+              value={authData.email}
+              onChange={handleChange}
+              className="w-full p-2 border border-gray-300 rounded-md bg-gray-50 text-black focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-800 font-semibold mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Your Password"
+              value={authData.password}
+              onChange={handleChange}
+              className="w-full p-2 border border-gray-300 rounded-md bg-gray-50 text-black focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-800 text-white px-6 py-3 rounded-md font-semibold hover:bg-blue-900 transition duration-300 w-full"
+          >
             {isLogin ? "Login" : "Signup"}
-          </h1>
-          <form onSubmit={handleSubmit}>
-            {!isLogin && (
-              <div className="mb-4">
-                <label htmlFor="name" className="block text-gray-800 font-semibold mb-2">
-                  Full Name
-
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  placeholder="Full Name"
-                  value={authData.name}
-                  onChange={handleChange}
-                  className="w-full p-2 border border-gray-600 rounded-md bg-gray-700 text-white"
-                />
-              </div>
-
-            )}
-            <div className="mb-4">
-              <label htmlFor="email" className="block text-gray-800 font-semibold mb-2">
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Your Email"
-                value={authData.email}
-                onChange={handleChange}
-                className="w-full p-2 border border-gray-300 rounded-md"
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="password" className="block text-gray-800 font-semibold mb-2">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Your Password"
-                value={authData.password}
-                onChange={handleChange}
-                className="w-full p-2 border border-gray-300 rounded-md"
-              />
-            </div>
-            <button
-              type="submit"
-              className="bg-blue-800 text-white px-6 py-3 rounded-md font-semibold hover:bg-blue-900 transition duration-300 w-full"
-            >
-              {isLogin ? "Login" : "Signup"}
-            </button>
-            <button
-              type="button"
-              className="px-2 py-3 border border-black bg-red-400 w-full mt-3"
-              onClick={loginWithGoogleHandler}
-            >
-              Login With Google
-            </button>
-            <button
-              type="button"
-              className="bg-gray-500 text-white px-6 py-3 rounded-md font-semibold hover:bg-gray-700 transition duration-300 w-full my-3"
-              onClick={toggleAuthMode}
-            >
-              {isLogin ? "Don't Have an Account? Signup" : "Already Have an Account? Login"}
-            </button>
-          </form>
-
-        </div>
+          </button>
+          <button
+            type="button"
+            className="px-2 py-3 border border-black bg-red-400 w-full mt-3"
+            onClick={loginWithGoogleHandler}
+          >
+            Login With Google
+          </button>
+          <button
+            type="button"
+            className="bg-gray-500 text-white px-6 py-3 rounded-md font-semibold hover:bg-gray-700 transition duration-300 w-full my-3"
+            onClick={toggleAuthMode}
+          >
+            {isLogin ? "Don't Have an Account? Signup" : "Already Have an Account? Login"}
+          </button>
+        </form>
       </div>
     </div>
   );
--- FrontEnd/src/components/Header.js
+++ FrontEnd/src/components/Header.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { signOutUser } from "../firebase/auth";
-import { setIdToken, setIsLogin, setLoginStatus } from "../store/authSlice";
+import { setIdToken, setLoginStatus } from "../store/authSlice";
 
 const Header = () => {
   const loginStatus = useSelector((state) => state.auth.loginStatus);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const logoutHandler = async () => {
     try {
@@ -15,47 +16,115 @@ const Header = () => {
       localStorage.removeItem("idToken");
       dispatch(setIdToken(null));
       dispatch(setLoginStatus());
-      navigate('/auth');
+      navigate("/auth");
     } catch (error) {
       console.log(error);
     }
   };
 
-  return (
-    <header className="bg-gradient-to-r from-neutral-900 to-zinc-600 shadow-lg ">
-      <div className="container mx-auto px-6 py-4 flex justify-between items-center">
-        <div className="text-3xl font-bold text-white">EduTech</div>
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-        <nav className="hidden md:flex space-x-8 text-2xl text-white">
-          <Link to="/" className="   transition duration-300 text-lg font-medium">
+  return (
+    <header className="bg-gradient-to-r from-neutral-900 to-zinc-600 shadow-lg fixed w-full top-0 mb-10 left-0 z-50">
+      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+        <div className="text-2xl md:text-3xl font-bold text-white">EduTech</div>
+
+        {/* Hamburger Menu Icon */}
+        <div className="md:hidden flex items-center">
+          <button
+            className="text-white focus:outline-none"
+            onClick={toggleMenu}
+          >
+            <svg
+              className="w-8 h-8"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d={menuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+              ></path>
+            </svg>
+          </button>
+        </div>
+
+        {/* Navigation Links */}
+        <nav
+          className={`${
+            menuOpen ? "block" : "hidden"
+          } md:flex space-x-8 text-lg md:text-2xl text-white absolute md:relative top-16 left-0 md:top-auto md:left-auto bg-neutral-900 md:bg-transparent w-full md:w-auto transition-transform duration-300`}
+        >
+          <Link
+            to="/"
+            className="block py-2 px-4 md:py-0 md:px-0 hover:text-gray-300 transition duration-300 text-lg font-medium"
+          >
             Home
           </Link>
-          <Link to="/courses" className="   transition duration-300 text-lg font-medium">
+          <Link
+            to="/courses"
+            className="block py-2 px-4 md:py-0 md:px-0 hover:text-gray-300 transition duration-300 text-lg font-medium"
+          >
             Courses
           </Link>
-          <Link to="/about" className="  transition duration-300 text-lg font-medium">
+          <Link
+            to="/about"
+            className="block py-2 px-4 md:py-0 md:px-0 hover:text-gray-300 transition duration-300 text-lg font-medium"
+          >
             About
           </Link>
-          <Link to="/contact" className="  transition duration-300 text-lg font-medium">
+          <Link
+            to="/contact"
+            className="block py-2 px-4 md:py-0 md:px-0 hover:text-gray-300 transition duration-300 text-lg font-medium"
+          >
             Contact
           </Link>
         </nav>
 
-        <div className="space-x-2">
+        {/* Authentication Buttons */}
+        <div className="space-x-2 hidden md:flex">
           {loginStatus ? (
             <button
               onClick={logoutHandler}
-              className="bg-gradient-to-r from-fuchsia-500 to-cyan-500  text-white px-4 py-2 rounded-md font-semibold hover:bg-blue-800 transition duration-300"
+              className="bg-gradient-to-r from-fuchsia-500 to-cyan-500 text-white px-4 py-2 rounded-md font-semibold hover:bg-blue-800 transition duration-300"
             >
               Logout
             </button>
           ) : (
-            <Link to="/auth" className="bg-gradient-to-r from-fuchsia-500 to-cyan-500  text-white px-4 py-2 rounded-md font-semibold hover:bg-blue-800 transition duration-300">
+            <Link
+              to="/auth"
+              className="bg-gradient-to-r from-fuchsia-500 to-cyan-500 text-white px-4 py-2 rounded-md font-semibold hover:bg-blue-800 transition duration-300"
+            >
               Login
             </Link>
           )}
-
         </div>
+      </div>
+
+      {/* Authentication Buttons for Mobile */}
+      <div className={`${
+            menuOpen ? "block" : "hidden"
+          } md:hidden w-full flex justify-end px-6 py-2`}>
+        {loginStatus ? (
+          <button
+            onClick={logoutHandler}
+            className="bg-gradient-to-r from-fuchsia-500 to-cyan-500 text-white px-4 py-2 rounded-md font-semibold hover:bg-blue-800 transition duration-300"
+          >
+            Logout
+          </button>
+        ) : (
+          <Link
+            to="/auth"
+            className="bg-gradient-to-r from-fuchsia-500 to-cyan-500 text-white px-4 py-2 rounded-md font-semibold hover:bg-blue-800 transition duration-300"
+          >
+            Login
+          </Link>
+        )}
       </div>
     </header>
   );
