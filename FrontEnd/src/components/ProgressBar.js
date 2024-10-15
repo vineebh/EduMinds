3e42cc1e02375ae6382f7ed952ef392a8c2b0ffb
Kuldeep2804Yadav <kuldeep2804yadav@gmail.com>
2024-10-16 00:54:10 +0530

    achivment and levelup

--- FrontEnd/src/components/ProgressBar.js
+++ FrontEnd/src/components/ProgressBar.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
-
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ProgressBar = ({ Level, course_title, total, courseLevel, C_ID }) => {
   const watchedVideos = useSelector((state) => state.progress.watchedVideos);
@@ -10,7 +11,8 @@ const ProgressBar = ({ Level, course_title, total, courseLevel, C_ID }) => {
   const [progress, setProgress] = useState(0);
   const [points, setPoints] = useState(0);
   const email = userInfo?.userID;
-  console.log(C_ID);
+  console.log(progress);
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -23,7 +25,7 @@ const ProgressBar = ({ Level, course_title, total, courseLevel, C_ID }) => {
 
         if (response.status === 200) {
           const userPoints = response.data.data.points;
-          console.log(userPoints);
+
           setPoints(userPoints);
         } else {
           console.warn(response.data.msg);
@@ -40,7 +42,6 @@ const ProgressBar = ({ Level, course_title, total, courseLevel, C_ID }) => {
     }
   }, [email, course_title]);
 
-  console.log(watchedVideos.length)
   useEffect(() => {
     if (total > 0) {
       const progressPercentage = Math.min(
@@ -53,8 +54,12 @@ const ProgressBar = ({ Level, course_title, total, courseLevel, C_ID }) => {
 
   const everyDayQuestionHandler = async () => {
     navigate("/everydayquestion", {
-      state: { C_ID, level: courseLevel, courseTitle:course_title },
+      state: { C_ID, level: courseLevel, courseTitle: course_title },
     });
+  };
+  
+  const levelUpHandler = () => {
+    navigate('/levelUp' ,{state :{C_ID, level:courseLevel, courseTitle:course_title}})
   };
 
   return (
@@ -85,11 +90,15 @@ const ProgressBar = ({ Level, course_title, total, courseLevel, C_ID }) => {
         >
           Everyday Question
         </button>
-        <button className="bg-yellow-500 px-4 py-2 rounded-full shadow-md hover:bg-yellow-600 transition-all">
-          Level Up
-        </button>
+        {progress === 100 && (
+          <button
+            className="bg-yellow-500 px-4 py-2 rounded-full shadow-md hover:bg-yellow-600 transition-all"
+            onClick={levelUpHandler}
+          >
+            Level Up
+          </button>
+        )}
       </div>
-
     </div>
   );
 };
